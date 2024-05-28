b673e436ad622d281fec4a8c7caa3ddb2e7cde2a
Aryan Bhardwaj <aryanbhardwaj1328@gmail.com>
2024-05-28 15:48:14 +0530

    display map using the leaflet library

--- script.js
+++ script.js
@@ -26,7 +26,19 @@ const inputElevation = document.querySelector(`.form__input--elevation`);
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude, longitude } = position.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coordinates = [latitude, longitude];
+
+    var map = L.map("map").setView(coordinates, 14);
+
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coordinates)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert(`Could not get your location!`);
